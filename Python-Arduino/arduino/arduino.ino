fa13d569a2e65540eda14460ecd7209da56ff06f
Aubin Sionville <aubin.sionville@stfrancoislaroche.fr>
2020-04-20 15:06:45 +0200

    Added Globals

---
+++ Python-Arduino/arduino/arduino.ino
@@ -0,0 +1,29 @@
+const int cpu_green_pin_1 = 5;
+const int cpu_green_pin_2 = 6;
+const int cpu_green_pin_3 = 7;
+const int cpu_yellow_pin_1 = 8;
+const int cpu_yellow_pin_2 = 9;
+const int cpu_yellow_pin_3 = 10;
+const int cpu_red_pin_1 = 2;
+const int cpu_red_pin_2 = 3;
+const int cpu_red_pin_3 = 4;
+
+const int gpu_green_pin_1 = 12;
+const int gpu_green_pin_2 = A0;
+const int gpu_green_pin_3 = A1;
+const int gpu_yellow_pin_1 = A2;
+const int gpu_yellow_pin_2 = A3;
+const int gpu_yellow_pin_3 = A4;
+const int gpu_red_pin_1 = A5;
+const int gpu_red_pin_2 = A6;
+const int gpu_red_pin_3 = A7;
+
+void setup() {
+  // put your setup code here, to run once:
+	Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
