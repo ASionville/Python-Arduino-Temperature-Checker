e375ee476f25b4d5da4061ad4eaba194e5c56143
Aubin Sionville <aubin.sionville@stfrancoislaroche.fr>
2020-04-20 14:26:30 +0200

    Commented

--- only_python.py
+++ only_python.py
@@ -5,36 +5,46 @@ import threading
 import time
 import wmi
 
+#We don't need to be faster
 WAIT_SECONDS = 1
 
+#Start the Software that gets the data
 def start_sensors():
 	subprocess.call(['OpenHardwareMonitor/OpenHardwareMonitor.exe'])
 
 def get_data():
-	pythoncom.CoInitialize()
-	w = wmi.WMI(namespace="OpenHardwareMonitor")
-	temperature_infos = w.Sensor()
+	
+	pythoncom.CoInitialize() #CoInitialize, beacause we have multiple threads
+
+	w = wmi.WMI(namespace="OpenHardwareMonitor") #Start the WMI
+
+	temperature_infos = w.Sensor() #Get all sensors data
+	#Initialize lists for dictionnary
 	names = []
 	values = []
 
+	#We're only caring about temperature
 	for sensor in temperature_infos:
 	    if sensor.SensorType==u'Temperature':
-	        #print(sensor.Name, end=" : ")
-	        #print(sensor.Value)
 	        names.append(sensor.Name)
 	        values.append(sensor.Value)
 
-	temperature_dict = dict(zip(names, values))
+	temperature_dict = dict(zip(names, values)) #Create a dictionnary with all the temperatures
+
+	#Getting CPU and GPU temperature only
 	temperature_cpu = temperature_dict["CPU Core"]
 	print(temperature_cpu)
 	temperature_gpu = temperature_dict["GPU Core"]
 	print(temperature_gpu)
 
+	#Repeat every WAIT_SECONDS
 	data_thread = threading.Timer(WAIT_SECONDS, get_data)
 	data_thread.start()
     
+#Start the thread for the WMI
 sensor_thread = threading.Thread(target = start_sensors)
 sensor_thread.start()
 
+#Let the WMI start before scanning for data
 time.sleep(3)
 get_data()
